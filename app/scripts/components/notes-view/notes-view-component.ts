2b0587930cdd98b789f7439a29b106055e2022e1
Wilf <wilf@blockforge.io>
2017-04-07 00:51:38 +0100

    get notes, get note(id) service functions using ngstorage

--- app/scripts/components/notes-view/notes-view-component.ts
+++ app/scripts/components/notes-view/notes-view-component.ts
@@ -1,13 +1,13 @@
 import * as angular from 'angular';
 import './notes-view.scss';
-
+import NotesService from '../../services/NotesService';
 
 class NotesViewComponent {
 
     note: any;
     newNote: String;
 
-    constructor (private $state: ng.ui.IStateProvider, private $http: ng.IHttpService) {
+    constructor (private $state: ng.ui.IStateProvider, private NotesService: NotesService) {
         // this.state = $state;
         this.init();
     }
@@ -15,39 +15,10 @@ class NotesViewComponent {
     private init(): void {
         console.log(this.$state.params.noteId)
         this.$state.params;
-        // api.getNote(this.state.params.noteId).then( (repsonse) => {
-            // this.note = response.data;
-        // })
+        this.NotesService.getNote(this.$state.params.noteId).then((response) => {
+            this.note = response;
+        })
 
-        function randomDate(start, end) {
-            return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-        }
-
-
-        this.note = {
-            id: 123456,
-            createdBy: 'Henry Jones',
-            title: 'This is a note',
-            date: randomDate(new Date(2017, 0, 1), new Date(2017, 2, 1)),
-            image: '/images/henry.jpg',
-            note: 'Lorem ipsum dolor sit amet, noluisse consectetuer ad vim, eu pro numquam partiendo, nam no modo labores appetere. Feugiat insolens deserunt ex sea, at debet argumentum pro, aliquam prodesset theophrastus ea sea. Ne qui sale nonumes, id facer scriptorem usu. Novum tamquam laoreet ea quo. Forensibus appellantur ea pri.',
-            notes: [
-                {
-                    id: 54782,
-                    createdBy: 'Sarah Folley',
-                    date: randomDate(new Date(2017, 2, 1), new Date()),
-                    image: '/images/sarah.jpg',
-                    note: 'Lorem ipsum dolor sit amet, eos id consul minimum, debet dicant vituperata est no, qui ad ocurreret gubergren. Vel ne porro feugiat philosophia, porro volutpat comprehensam est et. Commodo alienum copiosae in pri, in eam esse primis ponderum. Mei no wisi impetus disputationi, ex alii delicata sed. Ferri ipsum liber in sed, at vim labore volumus voluptaria, ei atomorum disputationi sea. Ex persius eleifend postulant eam, quo deserunt tractatos an.'
-                },
-                {
-                    id: 885574,
-                    createdBy: 'Henry Jones',
-                    date: randomDate(new Date(2017, 2, 1), new Date()),
-                    image: '/images/henry.jpg',
-                    note: 'Lorem ipsum dolor sit amet, te quo stet meliore, est te scaevola ponderum. Te enim copiosae euripidis his, etiam tritani albucius vix te? Quod vero putant mel te, nam sapientem intellegebat id, nec ne vidisse liberavisse. Habeo iuvaret vel an, eos ornatus pertinacia voluptatibus eu. Sed falli labitur eripuit ad, errem voluptatum est at, pri an meliore consectetuer.'
-                }
-            ]
-        };
     }
 
     private submitNote(): void {
--- app/scripts/services/NotesService.ts
+++ app/scripts/services/NotesService.ts
@@ -1,27 +1,82 @@
 import * as angular from 'angular';
 import 'ngstorage';
-
-const staticContent = true;
+// TODO put defaults in settings module
+// import Settings from '../../settings'
+//  --- private settings: Settings
 
 export default class NotesService {
+	useLocalStorage: Boolean = true;
+
 	static $inject = ['$localStorage', '$q']
-    constructor (private storage: angular.storage.IStorageService, private $q: ng.IQService) {}
+    constructor (private storage: angular.storage.IStorageService, private $q: ng.IQService) {
+    	this.init();
+    }
+
+    public init() {
+    	// TODO implement properly - use server db storage
+    	function randomDate(start, end) {
+    	    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    	}
+
+    	// Set example note in storage
+        if (this.useLocalStorage && !this.storage.notes) {
+        	this.storage.notes = [
+			    {   
+			        id: 123456,
+			        createdBy: 'Henry Jones',
+			        title: 'This is a note',
+			        date: randomDate(new Date(2017, 0, 1), new Date(2017, 2, 1)),
+			        image: '/images/henry.jpg',
+			        note: 'Lorem ipsum dolor sit amet, noluisse consectetuer ad vim, eu pro numquam partiendo, nam no modo labores appetere. Feugiat insolens deserunt ex sea, at debet argumentum pro, aliquam prodesset theophrastus ea sea. Ne qui sale nonumes, id facer scriptorem usu. Novum tamquam laoreet ea quo. Forensibus appellantur ea pri.',
+			        notes: [
+			            {
+			                id: 54782,
+			                createdBy: 'Sarah Folley',
+			                date: randomDate(new Date(2017, 2, 1), new Date()),
+			                image: '/images/sarah.jpg',
+			                note: 'Lorem ipsum dolor sit amet, eos id consul minimum, debet dicant vituperata est no, qui ad ocurreret gubergren. Vel ne porro feugiat philosophia, porro volutpat comprehensam est et. Commodo alienum copiosae in pri, in eam esse primis ponderum. Mei no wisi impetus disputationi, ex alii delicata sed. Ferri ipsum liber in sed, at vim labore volumus voluptaria, ei atomorum disputationi sea. Ex persius eleifend postulant eam, quo deserunt tractatos an.'
+			            },
+			            {
+			                id: 885574,
+			                createdBy: 'Henry Jones',
+			                date: randomDate(new Date(2017, 2, 1), new Date()),
+			                image: '/images/henry.jpg',
+			                note: 'Lorem ipsum dolor sit amet, te quo stet meliore, est te scaevola ponderum. Te enim copiosae euripidis his, etiam tritani albucius vix te? Quod vero putant mel te, nam sapientem intellegebat id, nec ne vidisse liberavisse. Habeo iuvaret vel an, eos ornatus pertinacia voluptatibus eu. Sed falli labitur eripuit ad, errem voluptatum est at, pri an meliore consectetuer.'
+			            }
+			        ]
+			    }
+			];
+        }
+
+    }
 
 
+    // TODO implement pagination - get only updated
 	public getNotes():ng.IPromise<{}> {
 		let defer = this.$q.defer();
-        if (staticContent) {
+        if (this.useLocalStorage) {
         	let notes = this.storage.notes;
         	defer.resolve(notes);
         } else {
-            throw new Error('Lost connection to the server');
+        	// TODO get from API - then set to storage
+            throw new Error('Can not update notes - Lost connection to the server');
         }
         return defer.promise;
 	}
 
-	public getNote(noteId): Object {
-		let note = {};
-		return note;
+	public getNote(noteId):ng.IPromise<{}> {
+		let defer = this.$q.defer();
+        if (this.useLocalStorage) {
+        	console.log(noteId);
+        	console.log(this.storage.notes[0].id);
+        	let note = this.storage.notes.filter( o => o.id == noteId);
+        	if (!note.length) throw new Error('Can not find note');
+        	defer.resolve(note[0]);
+        } else {
+        	// TODO get from API - then set to storage
+            throw new Error('Can not find notes - Lost connection to the server');
+        }
+        return defer.promise;
 	}
 
 	public postNote(noteObj): Object {
